20ed58c5255355a41a29cfbc5c337e79df4d80e6
Terrails <terrails.business@outlook.com>
2017-11-03 16:17:27 +0100

    axe fix

--- src/main/java/terrails/terracore/item/tool/AxeBase.java
+++ src/main/java/terrails/terracore/item/tool/AxeBase.java
@@ -1,27 +1,36 @@
 package terrails.terracore.item.tool;
 
+import com.google.common.collect.Sets;
 import com.mojang.realmsclient.gui.ChatFormatting;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
 import net.minecraft.world.World;
 import terrails.terracore.item.ItemBase;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Set;
 
-public class AxeBase extends ItemBase {
+public class AxeBase extends ItemTool {
 
     protected String name;
     protected String modid;
     protected boolean displayHarvestLevel;
     protected ToolMaterial material;
+    private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
+
 
     public AxeBase(ToolMaterial material, String name) {
-        super(name);
+        super(material, EFFECTIVE_ON);
         this.material = material;
         this.name = name;
+        setRegistryName(name);
+        setUnlocalizedName(name);
         setCreativeTab(null);
     }
 
